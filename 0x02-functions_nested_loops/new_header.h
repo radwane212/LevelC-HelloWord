91631cfd7d4757405e4bb66e639a618f18588cdf
radwane212 <redzmourine671@gmail.com>
2023-06-23 18:14:09 +0200

    0x02-functions_nested_loops/

---
+++ 0x02-functions_nested_loops/new_header.h
@@ -0,0 +1,8 @@
+#ifndef NEW_HEADER_H
+#define NEW_HEADER_H
+
+#include <stdio.h>  // Example system include
+
+// Function prototypes or other declarations can be added here
+
+#endif /* NEW_HEADER_H */
