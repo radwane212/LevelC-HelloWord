7d5b4d4d9d1a6fa9b638948d793e680ad3c131a6
radwane212 <redzmourine671@gmail.com>
2023-07-04 00:44:18 +0200

    pointers_arrays_strings/

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void simple_print_buffer(char *buffer, unsigned int size);
+void _memset(char *buffer, char value, unsigned int size);
+
+#endif /* MAIN_H */
